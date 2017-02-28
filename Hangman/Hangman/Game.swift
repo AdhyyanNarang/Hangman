d54f306e1282d73d1002b67a6de2d917b1847bc1
Adhyyan Narang <adhyyan@adhyyans-mbp.attlocal.net>
2017-02-28 03:04:49 -0800

    functionality completely incorporated, but UI is very poor

---
+++ Hangman/Hangman/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  Hangman
+//
+//  Created by Adhyyan Narang on 2/26/17.
+//  Copyright © 2017 Shawn D'Souza. All rights reserved.
+//
+
+import UIKit
+
+class Game: NSObject {
+
+}
